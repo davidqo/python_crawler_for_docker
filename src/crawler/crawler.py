27943a2a804d034b968384ee30555a40585709d2
davidqo <system.out@yandex.ru>
2017-01-26 07:13:21 +0000

    Page concatenator

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -1,14 +1,40 @@
+import re
+import codecs
 import sys
-from PyQt4 import QtGui
+import os
+import requests
+from selenium import webdriver
+from urlparse import urlparse
 
-app = QtGui.QApplication(sys.argv)
+if os.path.isfile("/etc/requests.txt"):
+	file = "/etc/requests.txt"
+else:
+	if os.path.isfile("~/requests.txt"):
+		file = "~/requests.txt"
+	else:
+		if os.path.isfile("./requests.txt"):
+			file = "./requests.txt"
+		else:
+			raise "no requests.txt specified"
+		 
 
-widget = QtGui.QWidget()
-widget.resize(250, 150)
-widget.setWindowTitle('simple')
-widget.show()
+	
 
-sys.exit(app.exec_())
+with open(file) as f:
+    request_list = f.readlines()
 
-import sys
-from PyQt4 import QtGui
+f = codecs.open('index.html', mode='w', encoding='utf-8')
+
+for req in request_list:
+	result = requests.get(req)
+	parsed_uri = urlparse(req)
+	domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+	page1 = re.sub("src=\"", "src=\"" + domain, result.text)
+	page2 = re.sub("href=\"", "href=\"" + domain, result.text)
+	f.write(page2)
+
+f.close()
+
+driver = webdriver.Firefox()
+driver.get("file://" + os.getcwd() + "/index.html")
+
